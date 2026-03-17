37394eedfb8a3e0eb7686278c77d2f1616d4b4fe
mohammadsohaibmoiz-cell <mohammadsohaibmoiz@gmail.com>
2026-03-17 07:38:33 +0000

    bhai

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -2,34 +2,37 @@ import { useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Demo } from './Component/onecomp'
 
 
 function App(){
   const[count,setcount] = useState(0)
-
+  const name="Sohaib"
+  let x=10;
+  let y=20;
+  function Name()
+  {
+   return "Mohammad Sohaib moiz";
+  }
+  function calc(c,d,x)
+  {
+    return cdx ;
+  }
+  function sum(a,b)
+  {
+    return a+b;
+  }
  return(
   <div>
-    <h1>ALI</h1>
-
-    <Emp></Emp>
-    <Branch></Branch>
-    <Demo></Demo>
+    <h1>project run</h1>
+    <h3>Name is {name}</h3>
+    <h3>{x+y}</h3>
+    <h1>Use of function with jsx</h1>
+    <h3>{Name()}</h3>
+    <h3>example if paramitized function{sum()}</h3>
+    <h3>{calc()}</h3>
     </div>
  )
-
 }
 
-function Emp(){
-  return(
-    <h1>Mohammad Sohaib Moiz</h1>
-  )
-}
-
-function Branch (){
- return(
- <h2>AIML</h2>
- )
-}
 
 export default App
