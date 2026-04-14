0dacb3c530d3a50a82b3397737cabe8dd9726a89
mohammadsohaibmoiz-cell <mohammadsohaibmoiz@gmail.com>
2026-04-14 07:38:38 +0000

    ali

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -6,30 +6,16 @@ import './App.css'
 
 function App(){
   const[count,setcount] = useState(0)
-  const name="Sohaib"
-  let x=10;
-  let y=20;
-  function Name()
+  const [data,setdata]=useState("hello")
+  function fun()
   {
-   return "Mohammad Sohaib moiz";
-  }
-  function calc(c,d,x)
-  {
-    return cdx ;
-  }
-  function sum(a,b)
-  {
-    return a+b;
+    setdata("check");
   }
  return(
   <div>
-    <h1>project run</h1>
-    <h3>Name is {name}</h3>
-    <h3>{x+y}</h3>
-    <h1>Use of function with jsx</h1>
-    <h3>{Name()}</h3>
-    <h3>example if paramitized function{sum()}</h3>
-    <h3>{calc()}</h3>
+    <h1>state of variable</h1>
+    <h1>{data}</h1>
+    <button onClick={fun}>Click </button>
     </div>
  )
 }
